bcb013b709513dc0f547ac0392e38cfe725d299a
Dayana Marcela Cogaria Boada <dayanacogaria@gmail.com>
2017-08-30 15:57:17 -0500

    pregunta y respuesta controlador

--- Controlador/preguntaController.php
+++ Controlador/preguntaController.php
@@ -1,12 +1,47 @@
 <?php
-
+require_once (__DIR__.'/../Modelo/Pregunta.php');
 /**
  * Created by PhpStorm.
  * User: usuario
  * Date: 29/08/2017
  * Time: 11:09 PM
  */
+if(!empty($_GET['action'])){
+    preguntaController::main($_GET['action']);
+}else{
+    echo "No se encontro ninguna accion...";
+}
 class preguntaController
 {
+
+    static function main($action){
+        if ($action == "crear"){
+            preguntaController::crear();
+        }/*else if ($action == "editar"){
+             preguntaController::editar();
+        }*/
+    }
+
+    static public function  crear()
+    {
+
+        try {
+            $arraypreg = array();
+            $arraypreg['Respuesta'] = $_POST['Respuesta'];
+            $arraypreg['Pregunta_IdPregunta'] = $_POST['Pregunta_IdPregunta'];
+            $arraypreg['Adopcion_IdAdopcion'] = $_POST['Adopcion_IdAdopcion'];
+
+            $Pregunta = new Pregunta ($arraypreg);
+            $Pregunta->insertar();
+            header("Location: ../Vista/pages/pregunta.php?respuesta=correcto");
+
+        } catch (Exception $e) {
+
+
+            header("Location: ../Vista/pages/pregunta.php?respuesta=error");
+        }
+
+
+    }
 
 }
---
+++ Controlador/respuestaController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: usuario
+ * Date: 29/08/2017
+ * Time: 11:26 PM
+ */
+class respuestaController
+{
+
+}
