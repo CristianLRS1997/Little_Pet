729c489bfa614ba1d0de2f969aab059af9a1bf81
CristianLRS1997 <clramirez576@misena.edu.co>
2017-08-30 12:17:54 -0500

    Cambios BD
    Eliminacion de campo envio

---
+++ Modelo/Adopcion.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: acer
+ * Date: 30/08/2017
+ * Time: 10:55
+ */
+require_once("db_abstract_class.php");
+
+class Adopcion extends db_abstract_class
+{
+  private $idAdopcion;
+  private $Estado;
+  private $
+}
