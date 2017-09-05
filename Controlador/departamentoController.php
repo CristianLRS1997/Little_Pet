9d54ab6e5018dbb9384e4bf3a88b0facbe2b680d
Dayana Marcela Cogaria Boada <dayanacogaria@gmail.com>
2017-09-05 14:51:52 -0500

    departamento

--- Controlador/departamentoController.php
+++ Controlador/departamentoController.php
@@ -20,12 +20,16 @@ class departamentoController
 
     }
 
-     static public function mostrar()
-    {
-
-
-
+    static public function mostrar ($isRequired=true, $id="IdDepartamento", $nombre="IdDepartamento", $class=""){
+        $arrdep= Departamento::getAll(); /*  */
+        $htmlSelect = "<select ".(($isRequired) ? "required" : "")." id= '".$id."' name='".$nombre."' class='".$class."'>";
+        $htmlSelect .= "<option >Seleccione</option>";
+        if(count($arrdep) > 0){
+            foreach ($arrdep as $dep)
+                $htmlSelect .= "<option value='".$dep->getIdDepartamento()."'>".$dep->getNombre()."</option>";
+        }
+        $htmlSelect .= "</select>";
+        return $htmlSelect;
     }
-
 
 }
