efabcb43f3ad8f0be8f4eeb0865208b39c70a914
CristianLRS1997 <clramirez576@misena.edu.co>
2017-09-11 21:23:59 -0500

    Wizard Registro Sujeto
    Diseño wizard

--- Vista/BackEnd/Adminnoxadmin-12/horizontal/registroSujeto.php
+++ Vista/BackEnd/Adminnoxadmin-12/horizontal/registroSujeto.php
@@ -54,144 +54,180 @@ header("Location: login.php");
                     ofertas de adopción para que el consiga el hogar que merece con calor de familia.
                 </p>
 
+
                 <br>
 
-                <form role="form" method="post" action="../../../../Controlador/sujetoAdopcionController.php?action=crear" >
-
-                    <div class="col-xs-9 center-page">
-
-                        <label>
-                            <input type="radio" name="Especie" value="op" checked />
-                            <img class="icon-colored" src="assets/images/icons/Perro.svg" title="Perros"/>
-                        </label>
-
-                        <label>
-                            <input type="radio" name="Especie" value="op" />
-                            <img class="icon-colored" src="assets/images/icons/Ave.svg" title="Aves"/>
-                        </label>
-                        <label>
-                            <input type="radio" name="Especie" value="op" />
-                            <img class="icon-colored" src="assets/images/icons/Gato.svg" title="Gatos"/>
-                        </label>
-
-                        <label>
-                            <input type="radio" name="Especie" value="op" />
-                            <img class="icon-colored" src="assets/images/icons/Pez.svg" title="Peces"/>
-                        </label>
-
-                        <label>
-                            <input type="radio" name="Especie" value="op" />
-                            <img class="icon-colored" src="assets/images/icons/Roedor.svg" title="Roedor"/>
-                        </label>
-
-                    </div>
+                <!-- Clickable Wizard -->
+                <div class="row">
+                    <div class="col-md-12">
 
 
+                        <form role="form" method="post" action="../../../../Controlador/sujetoAdopcionController.php?action=crear" >
 
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <input type="text" style="background-color: #FEF9E7 ; border:none" id="Nombre" name="Nombre" parsley-trigger="change" required
-                                   data-parsley-minlength="2" placeholder="Nombre de la mascota" class="form-control">
-                        </div>
 
-                        <div class="col-xs-6">
-                            <select style="background-color: #FEF9E7 ; border:none" id="Genero" name="Genero" class="form-control" required="">
-                                <option value="">Genero del Animal</option>
-                                <option value="Macho">Macho</option>
-                                <option value="Hembra">Hembra</option>
-                            </select>
-                        </div>
-                    </div>
+                        </form>
 
-                    <br>
 
-                    <div class="row">
-                        <div class="col-xs-6 col-md-3">
-                            <input style="background-color: #FEF9E7 ; border:none" type="number" id="Anos" name="Anos" class="form-control" required
-                                   data-parsley-minlength="1" placeholder="Años"/>
-                        </div>
+                        <form id="wizard-callbacks">
+                            <fieldset title="1">
+                                <legend>Información Basica</legend>
 
-                        <div class="col-xs-6 col-md-3">
-                            <input style="background-color: #FEF9E7 ; border:none" type="number" id="Meses" name="Meses" class="form-control" required
-                                   data-parsley-minlength="1" placeholder="Meses"/>
-                        </div>
+                                <div class="row m-t-20">
 
-                        <div class="col-xs-6">
-                            <select style="background-color: #FEF9E7; border:none" id="Tamano" name="Tamano" class="form-control" required="">
-                                <option value="">Tamaño</option>
-                                <option value="Grande">Grande</option>
-                                <option value="Mediano">Mediano</option>
-                                <option value="Pequeño">Pequeño</option>
-                                <option value="Mini">Mini</option>
-                            </select>
-                        </div>
-                    </div>
+                                        <div class="col-xs-9 center-page">
 
-                    <br>
+                                            <label>
+                                                <input type="radio" name="Especie" value="op" checked />
+                                                <img class="icon-colored" src="assets/images/icons/Perro.svg" title="Perros"/>
+                                            </label>
 
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <select style="background-color: #FEF9E7; border:none" id="Vacunas" name="Vacunas" class="form-control" required="">
-                                <option value="">Vacunas</option>
-                                <option value="Si">Si</option>
-                                <option value="No">No</option>
-                                <option value="No Reporta">No Reporta</option>
-                            </select>
-                        </div>
+                                            <label>
+                                                <input type="radio" name="Especie" value="op" />
+                                                <img class="icon-colored" src="assets/images/icons/Ave.svg" title="Aves"/>
+                                            </label>
+                                            <label>
+                                                <input type="radio" name="Especie" value="op" />
+                                                <img class="icon-colored" src="assets/images/icons/Gato.svg" title="Gatos"/>
+                                            </label>
 
-                        <div class="col-xs-3">
-                            <select style="background-color: #FEF9E7; border:none" id="Alimentacion" name="Alimentacion" class="form-control" required="">
-                                <option value="">Alimentación</option>
-                                <option value="Especial">Especial</option>
-                                <option value="Normal">Normal</option>
-                                <option value="No Sabe">No Sabe</option>
-                            </select>
-                        </div>
+                                            <label>
+                                                <input type="radio" name="Especie" value="op" />
+                                                <img class="icon-colored" src="assets/images/icons/Pez.svg" title="Peces"/>
+                                            </label>
 
-                        <div class="col-xs-6">
-                             <div class="radio radio-warning radio-inline">
-                                 <input type="radio" id="Si" value="Si" name="Esterilizado" checked>
-                                 <label for="Si">Esta esterilizado</label>
-                             </div>
-                             <div class="radio radio-warning radio-inline">
-                                 <input type="radio" id=No value="No" name="Esterilizado">
-                                 <label for="No"> Sin esterilizar </label>
-                             </div>
-                        </div>
-                    </div>
+                                            <label>
+                                                <input type="radio" name="Especie" value="op" />
+                                                <img class="icon-colored" src="assets/images/icons/Roedor.svg" title="Roedor"/>
+                                            </label>
 
-                    <br>
+                                        </div>
 
-                    <div class="form-group">
+                                        <div class="row">
+                                            <div class="col-xs-6">
+                                                <input type="text" style="background-color: #FEF9E7 ; border:none" id="Nombre" name="Nombre" parsley-trigger="change" required
+                                                       data-parsley-minlength="2" placeholder="Nombre de la mascota" class="form-control">
+                                            </div>
+
+                                            <div class="col-xs-6">
+                                                <select style="background-color: #FEF9E7 ; border:none" id="Genero" name="Genero" class="form-control" required="">
+                                                    <option value="">Genero del Animal</option>
+                                                    <option value="Macho">Macho</option>
+                                                    <option value="Hembra">Hembra</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <br>
+
+                                        <div class="row">
+                                            <div class="col-xs-6 col-md-3">
+                                                <input style="background-color: #FEF9E7 ; border:none" type="number" id="Anos" name="Anos" class="form-control" required
+                                                       data-parsley-minlength="1" placeholder="Años"/>
+                                            </div>
+
+                                            <div class="col-xs-6 col-md-3">
+                                                <input style="background-color: #FEF9E7 ; border:none" type="number" id="Meses" name="Meses" class="form-control" required
+                                                       data-parsley-minlength="1" placeholder="Meses"/>
+                                            </div>
+
+                                            <div class="col-xs-6">
+                                                <select style="background-color: #FEF9E7; border:none" id="Tamano" name="Tamano" class="form-control" required="">
+                                                    <option value="">Tamaño</option>
+                                                    <option value="Grande">Grande</option>
+                                                    <option value="Mediano">Mediano</option>
+                                                    <option value="Pequeño">Pequeño</option>
+                                                    <option value="Mini">Mini</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <br>
+
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <select style="background-color: #FEF9E7; border:none" id="Vacunas" name="Vacunas" class="form-control" required="">
+                                                    <option value="">Vacunas</option>
+                                                    <option value="Si">Si</option>
+                                                    <option value="No">No</option>
+                                                    <option value="No Reporta">No Reporta</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="col-xs-3">
+                                                <select style="background-color: #FEF9E7; border:none" id="Alimentacion" name="Alimentacion" class="form-control" required="">
+                                                    <option value="">Alimentación</option>
+                                                    <option value="Especial">Especial</option>
+                                                    <option value="Normal">Normal</option>
+                                                    <option value="No Sabe">No Sabe</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="col-xs-6">
+                                                <div class="radio radio-warning radio-inline">
+                                                    <input type="radio" id="Si" value="Si" name="Esterilizado" checked>
+                                                    <label for="Si">Esta esterilizado</label>
+                                                </div>
+                                                <div class="radio radio-warning radio-inline">
+                                                    <input type="radio" id=No value="No" name="Esterilizado">
+                                                    <label for="No"> Sin esterilizar </label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <br>
+
+                                        <div class="form-group">
                         <textarea style="background-color: #FEF9E7; border:none " id="Descripcion" class="form-control" name="Descripcion" required
                                   data-parsley-trigger="keyup" data-parsley-minlength="50"
                                   data-parsley-minlength-message="Debe terner almenos una descripción de 50 caracteres."
                                   data-parsley-validation-threshold="10" placeholder="Describe a tu mascota..."></textarea>
+                                        </div>
+
+                                    <br>
+
+                                    <div class="form-group row">
+                                        <div class="col-sm-8 col-sm-offset-4">
+                                            <button type="reset" class="btn waves-effect w-md waves-light btn-lg font-15" onmouseout="this.style.background ='#D2D2D2'"  onmouseover="this.style.background ='#8c8c8c'" style=" background-color: #D2D2D2 ; color: #fdfefe; border-radius: 5px">
+                                                <strong>Cancelar</strong>
+                                            </button>
+                                            <button type="submit" class="btn waves-effect w-md waves-light btn-lg font-15 " id="sa-success" value="validate" onmouseout="this.style.background ='#F5B041'"  onmouseover="this.style.background ='#F77C10'" style= "background-color: #F5B041 ; color: #fdfefe; border-radius: 5px">
+                                                <strong>Siguiente</strong>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+
+                            <fieldset title="2">
+                                <legend>Fotos de la Mascota</legend>
+
+                                <div class="p-20 p-b-0">
+                                    <p class="text-warning font-18">Fotos</p>
+                                    <div class="form-group clearfix">
+                                        <div class="col-sm-12 padding-left-0 padding-right-0">
+                                            <input type="file" name="files[]" id="filer_input1"
+                                                   required multiple="multiple">
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+
+                                <fieldset title="3">
+                                    <legend>Preguntas</legend>
+
+                                    <div class="row m-t-20">
+
+
+                                        <!-- dayana lo suyo -->
+
+                                    </div>
+                                </fieldset>
+
+                                <button type="submit" class="btn btn-primary stepy-finish">Submit</button>
+                            </form>
                     </div>
-
-                    <div class="p-20 p-b-0">
-                        <p class="text-warning font-18">Fotos</p>
-                        <div class="form-group clearfix">
-                            <div class="col-sm-12 padding-left-0 padding-right-0">
-                                <input type="file" name="files[]" id="filer_input1"
-                                       required multiple="multiple">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-sm-8 col-sm-offset-4">
-                            <button type="reset" class="btn waves-effect w-md waves-light btn-lg font-15" onmouseout="this.style.background ='#D2D2D2'"  onmouseover="this.style.background ='#8c8c8c'" style=" background-color: #D2D2D2 ; color: #fdfefe; border-radius: 5px">
-                                <strong>Cancelar</strong>
-                            </button>
-                            <button type="submit" class="btn waves-effect w-md waves-light btn-lg font-15 " id="sa-success" value="validate" onmouseout="this.style.background ='#F5B041'"  onmouseover="this.style.background ='#F77C10'" style= "background-color: #F5B041 ; color: #fdfefe; border-radius: 5px">
-                                <strong>Guardar</strong>
-                            </button>
-                        </div>
-                    </div>
-
-
-                </form>
+                </div>
+                <!-- End row -->
 
                 <div class="visible-lg" style="height: 79px;"></div>
             </div>
@@ -206,6 +242,11 @@ header("Location: login.php");
 
 
 <!-- FORM-VALIDATION.HTML -->
+
+<!--Form Wizard-->
+<script src="../plugins/jquery.stepy/jquery.stepy.min.js" type="text/javascript"></script>
+<!--wizard initialization-->
+<script src="assets/pages/jquery.wizard-init.js" type="text/javascript"></script>
 
 <!-- Parsley js -->
 <script type="text/javascript" src="../plugins/parsleyjs/parsley.min.js"></script>
