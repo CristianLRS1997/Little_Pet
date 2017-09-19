ae781748b4e48cc9ff07357ccf70a3cd2dd145ac
Dayana Marcela Cogaria Boada <dayanacogaria@gmail.com>
2017-09-19 01:01:04 -0500

    Controlador respuesta
    wcvedvc

--- Controlador/respuestaController.php
+++ Controlador/respuestaController.php
@@ -1,5 +1,8 @@
 <?php
 require_once (__DIR__.'/../Modelo/Respuesta.php');
+
+require_once (__DIR__.'/../Modelo/Adopcion.php');
+
 /**
  * Created by PhpStorm.
  * User: usuario
@@ -11,6 +14,8 @@ if(!empty($_GET['action'])){
 }else{
     echo "No se encontro ninguna accion...";
 }
+
+
 class respuestaController
 {
     static function main($action){
@@ -18,41 +23,165 @@ class respuestaController
             respuestaController::crear();
         }else if ($action == "mostrar"){
             respuestaController::mostrar();
+        }else if ($action == "crear2"){
+            respuestaController::crear2();
+        }else if ($action == "crear3"){
+            respuestaController::crear3();
+        }else if ($action == "crear4"){
+            respuestaController::crear4();
+        }else if ($action == "crear5"){
+            respuestaController::crear5();
+        }
+        else if ($action == "crear6"){
+            respuestaController::crear6();
         }
     }
 
     static public function crear()
     {
+
+
+
         try{
             $arrresp = array();
+            $arrresp['Enunciado']=$_POST['Enunciado'];
             $arrresp['Respuesta']=$_POST['Respuesta'];
-            $arrresp['Pregunta_IdPregunta']= $_POST['Pregunta_IdPregunta'];
             $arrresp['Adopcion_IdAdopcion']= "1";
             $resp = new Respuesta($arrresp);
-            $resp->insertar();
-            header("Location: ../Vista/BackEnd/Adminnoaxadmin-12/horizontal/mostrarpreguntas.php?respuesta=correcto");
+             $resp->insertar();
+           // var_dump($arrresp);
+            header("Location: ../Vista/BackEnd/Adminnoxadmin-12/horizontal/Pregunta2.php?respuesta=correcto");
         }catch (Exception $e){
            var_dump($e);
 
         }
     }
-
-     static public function mostrar()
+    static public function crear2()
     {
 
-            $arrrespuesta = Respuesta::getAll();
-            $htmlelement = "<h1>";
-            $htmlelement = "<p>";
 
 
-            foreach ($arrrespuesta as $valor ){
+        try{
+            $arrresp = array();
+            $arrresp['Enunciado']=$_POST['Enunciado'];
+            $arrresp['Respuesta']=$_POST['Respuesta'];
+            $arrresp['Adopcion_IdAdopcion']= "1";
+            $resp = new Respuesta($arrresp);
+            $resp->insertar();
+            // var_dump($arrresp);
+            header("Location: ../Vista/BackEnd/Adminnoxadmin-12/horizontal/Pregunta3.php?respuesta=correcto");
+        }catch (Exception $e){
+            var_dump($e);
 
-                $htmlelement .= "<h1>".$valor->getPregunta()."</h1>";
+        }
+    }
+    static public function crear3()
+    {
 
 
-            }
 
-return $htmlelement;
+        try{
+            $arrresp = array();
+            $arrresp['Enunciado']=$_POST['Enunciado'];
+            $arrresp['Respuesta']=$_POST['Respuesta'];
+            $arrresp['Adopcion_IdAdopcion']= "1";
+            $resp = new Respuesta($arrresp);
+            $resp->insertar();
+            // var_dump($arrresp);
+            header("Location: ../Vista/BackEnd/Adminnoxadmin-12/horizontal/Pregunta4.php?respuesta=correcto");
+        }catch (Exception $e){
+            var_dump($e);
+
+        }
+    }
+    static public function crear4()
+    {
+
+
+
+        try{
+            $arrresp = array();
+            $arrresp['Enunciado']=$_POST['Enunciado'];
+            $arrresp['Respuesta']=$_POST['Respuesta'];
+            $arrresp['Adopcion_IdAdopcion']= "1";
+            $resp = new Respuesta($arrresp);
+            $resp->insertar();
+            // var_dump($arrresp);
+            header("Location: ../Vista/BackEnd/Adminnoxadmin-12/horizontal/Pregunta5.php?respuesta=correcto");
+        }catch (Exception $e){
+            var_dump($e);
+
+        }
+    }
+    static public function crear5()
+    {
+
+
+
+        try{
+            $arrresp = array();
+            $arrresp['Enunciado']=$_POST['Enunciado'];
+            $arrresp['Respuesta']=$_POST['Respuesta'];
+            $arrresp['Adopcion_IdAdopcion']= "1";
+            $resp = new Respuesta($arrresp);
+            $resp->insertar();
+            // var_dump($arrresp);
+            header("Location: ../Vista/BackEnd/Adminnoxadmin-12/horizontal/Pregunta6.php?respuesta=correcto");
+        }catch (Exception $e){
+            var_dump($e);
+
+        }
+    }
+    static public function crear6()
+    {
+
+
+
+        try{
+            $arrresp = array();
+            $arrresp['Enunciado']=$_POST['Enunciado'];
+            $arrresp['Respuesta']=$_POST['Respuesta'];
+            $arrresp['Adopcion_IdAdopcion']= "1";
+            $resp = new Respuesta($arrresp);
+            $resp->insertar();
+            // var_dump($arrresp);
+            header("Location: ../Vista/BackEnd/Adminnoxadmin-12/horizontal/Index.php?respuesta=correcto");
+        }catch (Exception $e){
+            var_dump($e);
+
+        }
+    }
+
+
+
+    static public function mostrar()
+    {
+
+
+        $arrpreguntas =  Respuesta::getAll();
+        #$arrcampos = ["Enunciado","Respuesta1","Respuesta2","Respuesta3","Respuesta4"];
+        $htmlelemnt ="<p>";
+        $htmlelemnt .= "<br>";
+
+
+
+        foreach ($arrpreguntas as $pre){
+
+
+            $htmlelemnt .= "<h3 class='text-warning'>".$pre->getEnunciado()."</h3>";
+
+            $htmlelemnt.="<div class=\"checkbox checkbox-warning\">".
+                " <input id=\"checkbox2\" type=\"checkbox\" name='Respuesta' checked>".
+                "  <label for=\"checkbox2\">".
+                $pre->getRespuesta().
+                " </label>".
+                "</div>";
+
+
+        }
+        return $htmlelemnt;
+
+
 
     }
 
